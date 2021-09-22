e2c85f69a0d70d1f5845dfa15330021faba97988
MOHAMMADmiZAN <takbir.jcd@gmail.com>
2021-09-22 13:35:08 +0600

    Update

--- Controllers/DashboardController.go
+++ Controllers/DashboardController.go
@@ -1,11 +1,22 @@
 package Controllers
 
 import (
+	"fmt"
+	"html/template"
 	"net/http"
 )
 
 func Dashboard(w http.ResponseWriter, r *http.Request) {
 
-	renderGohtml(w, "dashboard.gohtml")
+	//renderGohtml(w, "dashboard.gohtml")
+	parseFiles, err := template.ParseFiles("View/dashboard.gohtml", "View/master.gohtml")
+	if err != nil {
+		fmt.Println(err.Error())
+	}
+	err = parseFiles.Execute(w, nil)
+	if err != nil {
+		fmt.Println(err.Error())
+
+	}
 
 }
