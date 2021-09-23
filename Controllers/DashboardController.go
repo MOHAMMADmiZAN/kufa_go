515660ac86ec4722fbcc9f62e57a77e1ef182b5b
MOHAMMADmiZAN <takbir.jcd@gmail.com>
2021-09-24 01:11:57 +0600

    Update

--- Controllers/DashboardController.go
+++ Controllers/DashboardController.go
@@ -11,14 +11,7 @@ func Dashboard(w http.ResponseWriter, r *http.Request, h httprouter.Params) {
 		"View/dashboard.gohtml",
 		"View/master.gohtml",
 	}
-	how := map[string]string{
-		"Name": "Mizan",
-	}
-	renderMultipleGohtml(w, templates, how)
-	//fmt.Println(template)
-	//files, err := template.ParseFiles("View/dashboard.gohtml", "View/master.gohtml")
-	//if err != nil {
-	//	log.Fatalln(err.Error())
-	//}
-	//files.Execute(w,how)
+
+	renderMultipleGohtml(w, templates, nil)
+
 }
