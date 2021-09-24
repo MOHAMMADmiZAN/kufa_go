8c12c16f45ebd8fc19530d608936682355815c0d
MOHAMMADmiZAN <takbir.jcd@gmail.com>
2021-09-24 22:31:22 +0600

    update

--- Controllers/DashboardController.go
+++ Controllers/DashboardController.go
@@ -6,11 +6,7 @@ import (
 )
 
 func Dashboard(w http.ResponseWriter, r *http.Request, h httprouter.Params) {
-	data := struct {
-		Name string
-	}{
-		Name: "Mizan",
-	}
-	renderMultipleGohtml(w, data, "View/Dashboard/dashboard.gohtml")
+
+	renderMultipleGohtml(w, nil, "View/Dashboard/dashboard.gohtml")
 
 }
